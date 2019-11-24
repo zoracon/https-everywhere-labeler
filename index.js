c3b8e215f8f3ef2c046f4eaceaba957647df4cf7
pipboy96 <pipboy96@protonmail.com>
2019-11-24 20:58:59 +0300

    Add index.js and action.yml

---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const core = require('@actions/core')
+const github = require('@actions/github')
+const minimatch = require('minimatch')
+
+const rulesetGlob = '/src/chrome/content/rules/*.xml'
+
+const labelNames = {
+  add: 'new-ruleset',
+  modify: 'ruleset-enhancement'
+}
+
+function isRuleset () {
+  return file => minimatch(file.name, rulesetGlob)
+}
+
+async function run () {
+  try {
+    const client = new github.GitHub(
+      core.getInput('repo-token', { required: true })
+    )
+
+    const context = github.context
+
+    const defaults = {
+      owner: context.repo.owner,
+      repo: context.repo.repo
+    }
+
+    if (context.payload.action !== 'opened' || !context.payload.pull_request) {
+      return
+    }
+
+    const prNumber = context.payload.pull_request
+
+    const fileList = await client.pulls.listFiles({
+      ...defaults,
+      pull_number: prNumber
+    })
+
+    if (!fileList.each(isRuleset)) {
+      // Don't touch PRs that modify anything except rulesets for now
+      return
+    }
+
+    const labels = []
+
+    if (fileList.each(file => file.status === 'added')) {
+      labels.push(labelNames.add)
+    } else {
+      labels.push(labelNames.modify)
+    }
+
+    if (labels.length !== 0) {
+      await client.issues.addLabels({
+        ...defaults,
+        issue_number: prNumber,
+        labels: labels
+      })
+    }
+  } catch (err) {
+    core.error(err.stack)
+    core.setFailed(err.message)
+  }
+}
+
+run()
